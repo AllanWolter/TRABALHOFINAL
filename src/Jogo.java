30a016bdb9c0a19b0db9fc47ff4961f6c7c01206
Eduardo Caramelo <eduardocaramelo164@gmail.com>
2026-06-10 10:06:55 -0300

    a

--- src/Jogo.java
+++ src/Jogo.java
@@ -181,8 +181,4 @@ public class Jogo {
                         "Armadilhas encontradas: " + armadilhasencontrados + "\n" + 
                         "Posições vazias exploradas: " + vaziosencontrados + "\n");
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 19fa41ec9f58ee61e19f1097aad3eb4bde2fdde5
